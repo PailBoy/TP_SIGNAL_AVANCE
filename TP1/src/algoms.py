8832fcc64e25c5cd1e003c6761ce867e83646663
PailBoy <pailboyer@gmail.com>
2025-10-15 16:38:08 +0200

    TP debut

--- TP1/src/algoms.py
+++ TP1/src/algoms.py
@@ -6,7 +6,7 @@ def algoms(x,d, P, mu) :
     w_tot = []
     erreur = [0]*N
     w_tot.append(w.copy())
-    for n in range(N):
+    for n in range(P-1, N):
         x_vec = []
         for p in range(P):
             x_vec.append(x[n-p] if n-p>=0 else 0)
@@ -19,6 +19,7 @@ def algoms(x,d, P, mu) :
             w[p]=w[p] + mu* erreur[n] * x_vec[p]
         w_tot.append(w.copy())
     return w_tot, y, erreur
+
 
 
 
