72a6fb20be406cef24802e7643a50a82f3c61fe2
PailBoy <pailboyer@gmail.com>
2025-10-15 11:18:50 +0200

    TP debut

---
+++ TP1/src/signals_utils.py
@@ -0,0 +1,35 @@
+import numpy as np
+from scipy.signal import lfilter
+
+def generate_test_signals(N=1000):
+    """
+    Génère les signaux x[n] (bruit blanc) et d[n] = h*x[n],
+    où h est la réponse impulsionnelle inconnue [1, 0.3, -0.1, 0.2].
+
+    Paramètres
+    ----------
+    N : int
+        Longueur du signal à générer
+    seed : int
+        Graine aléatoire pour la reproductibilité
+
+    Renvoie
+    -------
+    x : ndarray
+        Signal d'entrée (bruit blanc)
+    d : ndarray
+        Signal de sortie obtenu par filtrage
+    h : ndarray
+        Réponse impulsionnelle utilisée (filtre inconnu)
+    """
+
+    # Signal d'entrée : bruit blanc
+    x = np.random.randn(N)
+
+    # Réponse impulsionnelle "inconnue"
+    h = np.array([1.0, 0.3, -0.1, 0.2])
+
+    # Signal désiré obtenu par filtrage
+    d = lfilter(h, [1.0], x)
+
+    return x, d, h
